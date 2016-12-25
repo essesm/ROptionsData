94895a22819cafd6280c30b515188e9fe3858b3b
Michael Esses <emichael94@gmail.com>
2016-12-25 13:12:21 -0500

    Changed output path

--- scripts/get_Options_closest_to_Delta.sh
+++ scripts/get_Options_closest_to_Delta.sh
@@ -11,7 +11,7 @@ DATE=$(echo `date -d '+2 weeks' '+%Y%m%d'`'\ +'{1..7}'\ days' | xargs -n1 date -
 # Reformat the date to YYYYmmdd
 DATE=`date -d"$DATE" '+%Y%m%d'`
 
-OUTPUT="../output.$DATE.csv"
+OUTPUT="output.$DATE.csv"
 RECIPIENTS=$1
 
 Rscript get_Options_closest_to_Delta.R $DATE -0.25 ../input.csv $OUTPUT |&  mail -s "Options for $DATE" $RECIPIENTS -A $OUTPUT
